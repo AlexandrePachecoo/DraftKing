d3f16c72dc74717d5e5777c0595c80d2ba6014c1
Alexandre Sant`anna <78821541+AlexandrePachecoo@users.noreply.github.com>
2025-12-16 16:34:25 +0000

    adiciona arquivo  e conexão PGSQL

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
+const service = require("./src/modules/status/status.service");
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server está rodando na port ${PORT}`);
+app.listen(PORT, async () => {
+  const status = await service.getStatus();
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Status: ${JSON.stringify(status)}`);
 });
--- backend/tests/integration/status.test.js
+++ backend/tests/integration/status.test.js
@@ -1,4 +1,11 @@
+const { database } = require("../../src/infra/database");
+
 test("Get status returns 200 OK", async () => {
   const response = await fetch("http://localhost:3000/status");
   expect(response.status).toBe(200);
+});
+
+test("Database connection works", async () => {
+  const response = await database.query("SELECT 2+15 AS result;");
+  expect(response.rows[0].result).toBe(17);
 });
