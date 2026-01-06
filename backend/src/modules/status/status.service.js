f0c6c1c9c3f0ae8d6b8769097c4b940e16432afd
Alexandre Sant`anna <78821541+AlexandrePachecoo@users.noreply.github.com>
2026-01-06 04:43:50 +0000

    endpoint /status está finalizado

--- backend/src/modules/status/status.service.js
+++ backend/src/modules/status/status.service.js
@@ -1,5 +1,32 @@
+require("dotenv").config();
+const { database } = require("../../infra/database");
+
 async function getStatus() {
-  return { status: "Olá mundo" };
+  const updateAt = new Date().toISOString();
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = openedConnectionsResult.rows[0].count;
+
+  const versionPostgresResult = await database.query("SHOW server_version;");
+  const versionPostgres = versionPostgresResult.rows[0].server_version;
+
+  return {
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+        version: versionPostgres,
+      },
+    },
+  };
 }
 module.exports = {
   getStatus,
--- backend/tests/integration/status.test.js
+++ backend/tests/integration/status.test.js
@@ -3,6 +3,13 @@ require("dotenv").config();
 
 test("Get status returns 200 OK", async () => {
   const response = await fetch("http://localhost:3000/status");
+  const responseBody = await response.json();
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toEqual("18.1");
   expect(response.status).toBe(200);
 });
 
