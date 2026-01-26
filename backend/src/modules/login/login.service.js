bebbe02492746b86756d252fa4ccdc78d9a06262
Alexandre Sant`anna <78821541+AlexandrePachecoo@users.noreply.github.com>
2026-01-26 16:17:13 +0000

    Gera token JWT

--- backend/src/modules/login/login.service.js
+++ backend/src/modules/login/login.service.js
@@ -1,5 +1,28 @@
+const { database } = require("../../infra/database");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 async function postLogin(data) {
-  return {};
+  const { email, password } = data;
+  const query = `
+  SELECT id, email, password_hash
+  FROM users
+  WHERE email = $1
+  `;
+  const result = await database.query(query, [email]);
+  const user = result.rows[0];
+  const valid = await bcrypt.compare(password, user.password_hash);
+
+  if (valid === true) {
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+    return token;
+  } else {
+    return "Erro: email ou senha invalidos";
+  }
 }
 module.exports = {
   postLogin,
--- backend/tests/integration/register/post.test.js
+++ backend/tests/integration/register/post.test.js
@@ -2,20 +2,44 @@ const { database } = require("../../../src/infra/database");
 
 test("POST to register returns 200 OK", async () => {
   try {
-    const response = await fetch("http://localhost:3000/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: "teste",
-        email: "teste@teste.com",
-        password: "12345678",
-      }),
-    });
-    const responseBody = await response.json();
-    expect(responseBody.name).toBe("teste");
+    const resultRegister = await register();
+    const responseRegisterBody = await resultRegister.json();
+    expect(responseRegisterBody.name).toBe("teste");
+
+    const resultLogin = await login();
+    const responseLoginBody = await resultLogin.json();
+
+    expect(responseLoginBody).toContain("eyJh");
   } finally {
     await database.query(`DELETE FROM users WHERE email = 'teste@teste.com';`);
   }
 });
+
+async function register() {
+  const response = await fetch("http://localhost:3000/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "teste",
+      email: "teste@teste.com",
+      password: "12345678",
+    }),
+  });
+  return response;
+}
+
+async function login() {
+  const response = await fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: "teste@teste.com",
+      password: "12345678",
+    }),
+  });
+  return response;
+}
