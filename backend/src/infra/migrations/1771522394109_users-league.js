0fcffc4f07cba2c334d6dfe99a537bcae8c261f6
Alexandre Sant`anna <78821541+AlexandrePachecoo@users.noreply.github.com>
2026-02-19 17:39:48 +0000

    cria a tabela users_league

---
+++ backend/src/infra/migrations/1771522394109_users-league.js
@@ -0,0 +1,44 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("users_league", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    id_user: {
+      type: "uuid",
+      notNull: true,
+      references: '"users"',
+      onDelete: "cascade",
+    },
+    id_league: {
+      type: "uuid",
+      notNull: true,
+      references: '"leagues"',
+      onDelete: "cascade",
+    },
+    cargo: {
+      type: "varchar(100)",
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("users_league");
+};
