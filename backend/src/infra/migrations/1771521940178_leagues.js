54949188fa7469c89172d6f4301b8a3d5280a51b
Alexandre Sant`anna <78821541+AlexandrePachecoo@users.noreply.github.com>
2026-02-19 17:31:26 +0000

    cria a tabela league no banco

---
+++ backend/src/infra/migrations/1771521940178_leagues.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("leagues", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    name: {
+      type: "varchar(150)",
+      notNull: true,
+      unique: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("leagues");
+};
